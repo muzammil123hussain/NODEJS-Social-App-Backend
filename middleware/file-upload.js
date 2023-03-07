788d845d0660b66854ff75866738b95b13c76301
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2023-03-07 14:18:34 +0500

    multer start

---
+++ middleware/file-upload.js
@@ -0,0 +1,5 @@
+import multer from "multer";
+
+const fileUpload = multer({});
+
+module.exports = fileUpload;
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const { getUsers, signup, login } = require("../controllers/users-controller");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ router.get("/", getUsers);
 
 router.post(
   "/signup",
+  fileUpload.sinlge("image"),
   [
     check("name").notEmpty(),
     check("email").normalizeEmail().isEmail(),
