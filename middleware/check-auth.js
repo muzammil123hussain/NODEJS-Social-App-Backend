7580c24eab1f64ed451a49c52edf9990d6f47168
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2023-03-09 23:50:53 +0500

    supace the options request

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,6 +2,9 @@ const HttpError = require("../models/http-error");
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
   try {
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
